9ea3879b8bfe7167d453ff8aa8f7127db0650826
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:42:58 +0530

    Create 2802-FindThePunishmentNumberOfAnInteger

---
+++ 2802-FindThePunishmentNumberOfAnInteger/2802-FindThePunishmentNumberOfAnInteger.java
@@ -0,0 +1,12 @@
+// Last updated: 8/6/2025, 4:42:21 PM
+class Solution {
+    public int punishmentNumber(int n) {
+        int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=n) sum += arr[i]*arr[i];
+            else if(arr[i]>n) break;
+        }
+        return sum;
+    }
+}
