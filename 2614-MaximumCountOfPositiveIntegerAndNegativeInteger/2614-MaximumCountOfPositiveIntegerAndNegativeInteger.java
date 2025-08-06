e75e1c25b75e9750e423fd07b053751c4de1091c
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:43:19 +0530

    Create 2614-MaximumCountOfPositiveIntegerAndNegativeInteger

---
+++ 2614-MaximumCountOfPositiveIntegerAndNegativeInteger/2614-MaximumCountOfPositiveIntegerAndNegativeInteger.java
@@ -0,0 +1,15 @@
+// Last updated: 8/6/2025, 4:42:42 PM
+class Solution {
+    public int maximumCount(int[] nums) {
+        int p=0,n=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                p++;
+            }
+            else if(nums[i]<0){
+                n++;
+            }
+        }
+        return Math.max(p,n);
+    }
+}
