e3ced3955999ea6d723e03d28f11c8f10b189496
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:49:52 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,22 @@
+// Last updated: 8/6/2025, 4:49:15 PM
+class Solution {
+    public boolean isHappy(int n) {
+        if(n<0) return false;
+        int slow=n,fast=n;
+        do{
+            slow=square(slow);
+            fast=(square(square(fast)));
+        }while(slow!=fast);
+        
+        return slow==1;
+    }
+    private int square(int x){
+        int sq=0;
+        while(x>0){
+            int d=x%10;
+            sq+=d*d;
+            x/=10;
+        }
+        return sq;
+    }
+}
