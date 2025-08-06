0878fe4f223b700830eeb9c365f085c18ebd1565
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:49:43 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.java
@@ -0,0 +1,11 @@
+// Last updated: 8/6/2025, 4:49:05 PM
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }}
