c3347e34234cb5d811c6bc07091db1ed7ce56b23
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:47:48 +0530

    Create 696-CountBinarySubstrings

---
+++ 696-CountBinarySubstrings/696-CountBinarySubstrings.java
@@ -0,0 +1,15 @@
+// Last updated: 8/6/2025, 4:47:11 PM
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int cur = 1, pre = 0, res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) cur++;
+            else {
+                res += Math.min(cur, pre);
+                pre = cur;
+                cur = 1;
+            }
+        }
+        return res + Math.min(cur, pre);
+    }
+}
