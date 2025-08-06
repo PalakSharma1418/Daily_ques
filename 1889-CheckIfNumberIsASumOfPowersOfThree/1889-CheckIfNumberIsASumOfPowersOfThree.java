731996795295162b8becbb3bd7f60e4fc6fe7530
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:45:30 +0530

    Create 1889-CheckIfNumberIsASumOfPowersOfThree

---
+++ 1889-CheckIfNumberIsASumOfPowersOfThree/1889-CheckIfNumberIsASumOfPowersOfThree.java
@@ -0,0 +1,12 @@
+// Last updated: 8/6/2025, 4:44:52 PM
+class Solution {
+    public boolean checkPowersOfThree(int n) {
+        while(n>0){
+            if(n%3==2){
+                return false;
+            }
+            n/=3;
+        }
+        return true;
+    }
+}
