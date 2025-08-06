076d692795ed3ebad957e3d39d58553ada75e5ba
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:50:17 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.java
@@ -0,0 +1,56 @@
+// Last updated: 8/6/2025, 4:49:39 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode mid=middle(head);
+        ListNode headB=mid.next;
+        mid.next=null;
+        ListNode a=sortList(head);
+        ListNode b=sortList(headB);
+        return Merge(a,b);
+    }
+    public ListNode Merge(ListNode list1, ListNode list2){
+        ListNode dummy=new ListNode();
+        ListNode temp=dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                dummy.next=list1;
+                dummy=dummy.next;
+                list1=list1.next;
+            }
+            else{
+                dummy.next=list2;
+                dummy=dummy.next;
+                list2=list2.next;
+            }
+        }
+        if(list1==null){
+            dummy.next=list2;
+        }
+        if(list2==null){
+            dummy.next=list1;
+        }
+        return temp.next;
+    }
+    public ListNode middle(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return slow;
+    }
+}
