abf1848278310d585173a74f387097fdd7dd1a16
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:49:45 +0530

    Create 215-KthLargestElementInAnArray

---
+++ 215-KthLargestElementInAnArray/215-KthLargestElementInAnArray.java
@@ -0,0 +1,15 @@
+// Last updated: 8/6/2025, 4:49:08 PM
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();        
+    }
+}
