5c647485878e663b329eea1bf0df476f3fae45db
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:43:04 +0530

    Create 2695-FindScoreOfAnArrayAfterMarkingAllElements

---
+++ 2695-FindScoreOfAnArrayAfterMarkingAllElements/2695-FindScoreOfAnArrayAfterMarkingAllElements.java
@@ -0,0 +1,19 @@
+// Last updated: 8/6/2025, 4:42:26 PM
+class Solution {
+    public long findScore(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        int l = 0, r = 0;
+        while(r<n){
+            l=r;
+            while(r+1<n && nums[r]>nums[r+1]){
+                r++;
+            }
+            for (int i = r; i >= l; i -= 2) {
+                sum += nums[i];
+            }
+            r += 2;
+        }
+        return sum;
+    }
+}
