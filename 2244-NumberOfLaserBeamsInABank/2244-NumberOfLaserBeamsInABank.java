e6f1c4547782eb5f73aa5486ea2e2732330d92f0
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:44:21 +0530

    Create 2244-NumberOfLaserBeamsInABank

---
+++ 2244-NumberOfLaserBeamsInABank/2244-NumberOfLaserBeamsInABank.java
@@ -0,0 +1,17 @@
+// Last updated: 8/6/2025, 4:43:43 PM
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int ans = 0, pre = 0;
+        for (int i = 0;i < bank.length; i ++) {
+            int n = 0;
+            for (int j = 0; j < bank[i].length(); j ++) 
+            {
+                if(bank[i].charAt(j) == '1') n ++;
+            }
+            if (n == 0) continue;
+            ans += pre * n;;
+            pre = n;
+        }
+        return ans;
+    }
+}
