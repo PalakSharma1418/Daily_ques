a971a4b7fe8f8c8f11cee9cf65fec2f924d4137a
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:48:16 +0530

    Create 523-ContinuousSubarraySum

---
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.java
@@ -0,0 +1,25 @@
+// Last updated: 8/6/2025, 4:47:38 PM
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> h=new HashMap<>();
+        h.put(0,-1);
+        int prefixSum=0,remainder=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            prefixSum+=nums[i];
+            remainder=prefixSum%k;
+            // if(remainder==0){
+            //     return true;
+            // }
+            if(h.containsKey(remainder))
+            {
+                int index=h.get(remainder);
+                if(i-index>1)
+                    return true;
+            }
+            else
+                h.put(remainder,i);
+        }
+        return false;
+    }
+}
