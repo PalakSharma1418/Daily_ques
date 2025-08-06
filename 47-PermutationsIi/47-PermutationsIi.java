f9172f333c483081138e60c6f8867f93990d3e66
PalakSharma1418 <palaksharma1418@gmail.com>
2025-08-06 16:51:34 +0530

    Create 47-PermutationsIi

---
+++ 47-PermutationsIi/47-PermutationsIi.java
@@ -0,0 +1,28 @@
+// Last updated: 8/6/2025, 4:50:57 PM
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+       List<List<Integer>> f = new ArrayList<>();
+        List<Integer> ll = new ArrayList<>();
+        boolean[] used = new boolean[nums.length]; // Track used elements
+        permutation(nums, ll, f, used);
+        return f;
+    }
+    public static void permutation(int[] nums, List<Integer> ll, List<List<Integer>> f, boolean[] used) {
+        if (ll.size() == nums.length) {
+            f.add(new ArrayList<>(ll));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if(i > 0 && (nums[i - 1] == nums[i]) && used[i - 1] == false) 
+                continue;
+            if (!used[i]) { // Ensure each number is used only once
+                used[i] = true;
+                ll.add(nums[i]);
+                permutation(nums, ll, f, used);
+                ll.remove(ll.size() - 1);
+                used[i] = false;
+            }
+        }
+    }
+}
