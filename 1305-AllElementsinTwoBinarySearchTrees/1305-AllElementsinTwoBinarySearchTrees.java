9ff33eff556d91a9a530576b5bf1c6e97922f37a
Palak Sharma <palaksharma1418@gmail.com>
2025-08-24 08:20:34 +0530

    Create 1305-AllElementsinTwoBinarySearchTrees

---
+++ 1305-AllElementsinTwoBinarySearchTrees/1305-AllElementsinTwoBinarySearchTrees.java
@@ -0,0 +1,35 @@
+// Last updated: 8/24/2025, 8:19:46 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> ll = new ArrayList<>();
+        add(root1,ll);
+        add(root2,ll);
+        Collections.sort(ll);
+        return ll;
+    }
+    public void add(TreeNode root , List<Integer> ll){
+        if(root==null ) return;
+        // if(root.left == null && root.right == null){
+            ll.add(root.val);
+        //     return;
+        // }
+
+        add(root.left,ll);
+        add(root.right,ll);
+    }
+}
