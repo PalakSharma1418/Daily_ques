1030adf6601e0de6f4dbbb6985fd3f9208f3cc76
Palak Sharma <palaksharma1418@gmail.com>
2025-08-28 11:19:47 +0530

    File updated at 8/28/2025, 11:18:57 AM

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,21 +1,31 @@
-// Last updated: 8/6/2025, 4:48:51 PM
+// Last updated: 8/28/2025, 11:18:57 AM
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int [] sa= new int[26];
-        int [] ta= new int[26];
-        for(int i=0;i<s.length();i++){
-            sa[s.charAt(i)-'a']++;
-            ta[t.charAt(i)-'a']++;
-        }
-
-        for(int i=0;i<26;i++){
-            if(sa[i]!=ta[i]){
-                return false;
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(int i=0;i<strs.length;i++){
+            String k=getkey(strs[i]);
+            if(!map.containsKey(k)){
+                map.put(k,new ArrayList<>());
             }
+            map.get(k).add(strs[i]);
         }
-        return true;
+        // return new ArrayList<>(map.values());
+        List<List<String>> ll = new ArrayList<>();
+        for(String key : map.keySet()){
+            ll.add(map.get(key));
+        }
+        return ll;
+    }
+    public String getkey(String s){
+        int[] fr=new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            fr[ch-'a']++;
+        }
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<fr.length;i++){
+            sb.append(fr[i]).append('#');
+        }
+        return sb.toString();
     }
 }
