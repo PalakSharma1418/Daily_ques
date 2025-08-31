5dc012ef7059fe2580812f95489e4ae28e13303d
Palak Sharma <palaksharma1418@gmail.com>
2025-08-31 16:28:56 +0530

    Create 1008-BinaryTreeCameras

---
+++ 1008-BinaryTreeCameras/1008-BinaryTreeCameras.java
@@ -0,0 +1,46 @@
+// Last updated: 8/31/2025, 4:28:03 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int camera=0;
+    public int minCameraCover(TreeNode root) {
+          int c= minCamera(root);
+          if(c == -1){
+            camera++;
+          }
+          return camera;
+    }
+    public int minCamera(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int left = minCamera(root.left);
+        int right = minCamera(root.right);
+        if(left ==-1 || right ==-1){//is node par need hai camera
+            camera++;
+            return 1; // camera setup kara hai yha
+        }
+        if(left==1 || right==1){ 
+            // inmai se koi ek pass camera ya dono ke pass hai and dusra bala covered hai
+            return 0;//covered
+        }
+        else{
+            return -1;
+            //kyuki dono child to cover hai lekin mujhe requirement hai
+        }
+    }
+}
