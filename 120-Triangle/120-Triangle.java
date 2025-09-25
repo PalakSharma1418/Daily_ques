322231879d07e5d39a413d83f22be551057305e1
Palak Sharma <palaksharma1418@gmail.com>
2025-09-25 16:44:03 +0530

    File updated at 9/25/2025, 4:42:55 PM

--- 120-Triangle/120-Triangle.java
+++ 120-Triangle/120-Triangle.java
@@ -1,28 +1,26 @@
-// Last updated: 9/25/2025, 4:42:32 PM
+// Last updated: 9/25/2025, 4:42:55 PM
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[][] dp = new int[n][n];
         for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], Integer.MAX_VALUE); // mark unvisited
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-        return min(triangle, 0, 0, 0, dp);
+        return min(triangle, 0, 0, dp);
     }
 
-    public int min(List<List<Integer>> triangle, int col, int row, int sum, int[][] dp) {
-        if (row == triangle.size()) {
-            return sum;
+    public int min(List<List<Integer>> triangle, int row, int col, int[][] dp) {
+        if (row == triangle.size() - 1) {
+            return triangle.get(row).get(col);
         }
-
         if (dp[row][col] != Integer.MAX_VALUE) {
-            return sum + dp[row][col];
+            return dp[row][col];
         }
 
-        int a = min(triangle, col, row + 1, sum + triangle.get(row).get(col), dp);
-        int b = min(triangle, col + 1, row + 1, sum + triangle.get(row).get(col), dp);
+        int curr = triangle.get(row).get(col);
+        int a = curr + min(triangle, row + 1, col, dp);
+        int b = curr + min(triangle, row + 1, col + 1, dp);
 
-        dp[row][col] = Math.min(a, b) - sum; // hume phele ki kya jarurat hum to yha se bottom tak ka store kar rhe hai isliye delte kiya
-
-        return Math.min(a, b);
+        return dp[row][col] = Math.min(a, b);
     }
 }
