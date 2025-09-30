5aee962da78027d4a18b75503f8816b813e97052
Palak Sharma <palaksharma1418@gmail.com>
2025-09-30 20:31:25 +0530

    File updated at 9/30/2025, 8:30:15 PM

--- 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
+++ 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.java
@@ -1,18 +1,34 @@
-// Last updated: 9/30/2025, 8:28:58 PM
+// Last updated: 9/30/2025, 8:30:15 PM
 class Solution {
-    public int[][] flipAndInvertImage(int[][] image) {
-        for(int i=0;i<image.length;i++){
-            int j=0;
-            int k=image[i].length-1;
-            while(j<=k){
-                int temp=image[i][j]^1;
-                image[i][j]=image[i][k]^1;
-                image[i][k]=temp;
-                j++;
-                k--;
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> ll = new ArrayList<>();
+        int[] row = new int[matrix.length];
+        int [] col = new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=0;j<matrix[0].length;j++){
+                if(min> matrix[i][j]){
+                    min=matrix[i][j];
+                }
             }
-            
+            row[i]=min;
         }
-        return image;
+        for(int j=0;j<matrix[0].length;j++){
+            int max=Integer.MIN_VALUE;
+            for(int i=0;i<matrix.length;i++){
+                if(max< matrix[i][j]){
+                    max=matrix[i][j];
+                }
+            }
+            col[j]=max;
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==row[i] && matrix[i][j]==col[j]){
+                    ll.add(matrix[i][j]);
+                }
+            }
+        }
+        return ll;
     }
 }
