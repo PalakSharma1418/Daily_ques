6af64320b3667bc4beff43a3bf2107e140d7314d
Palak Sharma <palaksharma1418@gmail.com>
2025-10-26 18:48:57 +0530

    Create 2294-MinimumTimeToCompleteTrips

---
+++ 2294-MinimumTimeToCompleteTrips/2294-MinimumTimeToCompleteTrips.java
@@ -0,0 +1,29 @@
+// Last updated: 10/26/2025, 6:47:34 PM
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
+        long ans=0;
+        int max = 0;
+        for(int n:time){
+            max=Math.max(n,max);
+        }
+        long low = 1, high = (long) max * totalTrips;
+        while(low<=high){
+            long mid=low+(high-low)/2;
+            if(ispossible(time,mid,totalTrips)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+    public boolean ispossible(int[] time, long t, int trip) {
+    long sum = 0;
+    for (int i = 0; i < time.length; i++) {
+        sum += t / time[i];
+        if (sum >= trip) return true; 
+    }
+    return sum >= trip;
+}
+}
