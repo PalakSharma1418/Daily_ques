668e3f9d574263d6bafbd276ee9cf2c2922d83cf
Palak Sharma <palaksharma1418@gmail.com>
2025-11-08 18:39:36 +0530

    Create 1611-MinimumOneBitOperationstoMakeIntegersZero

---
+++ 1611-MinimumOneBitOperationstoMakeIntegersZero/1611-MinimumOneBitOperationstoMakeIntegersZero.java
@@ -0,0 +1,11 @@
+// Last updated: 11/8/2025, 6:38:09 PM
+class Solution {
+    public int minimumOneBitOperations(int n) {
+        int result = 0;
+        while (n > 0) {
+            result ^= n;
+            n = n >> 1;
+        }    
+        return result;
+    }
+}
