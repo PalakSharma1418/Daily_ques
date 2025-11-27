2d803d3bd75ed12198a59de0752501473b15ba49
Palak Sharma <palaksharma1418@gmail.com>
2025-11-27 12:18:01 +0530

    File updated at 11/27/2025, 12:16:27 PM

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,16 +1,14 @@
-// Last updated: 8/6/2025, 4:49:47 PM
-class Solution {
-    public int singleNumber(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
-        for(int i=0;i<n;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
-        for(Integer key:map.keySet()){
-            if(map.get(key)==1){
-                return key;
-            }
-        }
-        return -1; 
-    }
-}
+// Last updated: 11/27/2025, 12:16:27 PM
+1class Solution {
+2    public int singleNumber(int[] nums) {
+3        int n=nums.length;
+4        int xor =0;
+5        for(int i=0;i<n;i++){
+6            xor=xor^nums[i];
+7            // xor=xor^(i+1);
+8        }
+9        // xor=xor^(n+1);
+10        return xor;
+11    }
+12}
+13// xor le lo 
