d2dcaac53c866e74d2264b73591133ed6c78769e
Palak Sharma <palaksharma1418@gmail.com>
2025-12-09 19:32:48 +0530

    File updated at 12/9/2025, 7:31:05 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,14 +1,14 @@
-// Last updated: 8/6/2025, 4:51:50 PM
-class Solution {
-    public static int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-             for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                   int [] a={i,j};
-                   return a;
-                }
-             }
-        }
-        return new int[0];
-}
-}
+// Last updated: 12/9/2025, 7:31:05 PM
+1class Solution {
+2    public static int[] twoSum(int[] nums, int target) {
+3        for(int i=0;i<nums.length;i++){
+4             for(int j=i+1;j<nums.length;j++){
+5                if(nums[i]+nums[j]==target){
+6                   int [] a={i,j};
+7                   return a;
+8                }
+9             }
+10        }
+11        return new int[0];
+12}
+13}
