c9bd3b7a292b7820b93486b33f7c893b99cb54fa
Palak Sharma <palaksharma1418@gmail.com>
2025-12-11 07:34:12 +0530

    Create 4119-MinimumDistanceBetweenThreeEqualElementsIi

---
+++ 4119-MinimumDistanceBetweenThreeEqualElementsIi/4119-MinimumDistanceBetweenThreeEqualElementsIi.java
@@ -0,0 +1,24 @@
+// Last updated: 12/11/2025, 7:32:33 AM
+class Solution {
+    public int minimumDistance(int[] nums) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            map.computeIfAbsent(nums[i] , x -> new ArrayList<>()).add(i);
+        }
+
+        for(List<Integer> pos : map.values()){
+            if(pos.size()>=3){
+                for(int i=0;i+2<pos.size();i++){
+                    int a = pos.get(i);
+                    int c =pos.get(i+2);
+                    int d = 2*(c-a);
+                    ans=Math.min(ans,d);
+                }
+            }
+        }
+
+        return ans==Integer.MAX_VALUE?-1:ans;
+        
+    }
+}
