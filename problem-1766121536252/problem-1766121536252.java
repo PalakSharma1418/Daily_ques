0b810c7295958a033f1ad1c29dd2a087db67177c
Palak Sharma <palaksharma1418@gmail.com>
2025-12-19 10:50:38 +0530

    Create

---
+++ problem-1766121536252/problem-1766121536252.java
@@ -0,0 +1,14 @@
+// Last updated: 12/19/2025, 10:48:56 AM
+1class Solution {
+2    public static int[] twoSum(int[] nums, int target) {
+3        for(int i=0;i<nums.length;i++){
+4             for(int j=i+1;j<nums.length;j++){
+5                if(nums[i]+nums[j]==target){
+6                   int [] a={i,j};
+7                   return a;
+8                }
+9             }
+10        }
+11        return new int[0];
+12}
+13}
