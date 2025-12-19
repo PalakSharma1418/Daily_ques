31e3a6603cc756287eea63b28e5b13f0ed4682b8
Palak Sharma <palaksharma1418@gmail.com>
2025-12-19 19:56:59 +0530

    Create 594-LongestHarmoniousSubsequence

---
+++ 594-LongestHarmoniousSubsequence/594-LongestHarmoniousSubsequence.java
@@ -0,0 +1,19 @@
+// Last updated: 12/19/2025, 7:55:16 PM
+1class Solution {
+2    public int findLHS(int[] nums) {
+3        HashMap<Integer, Integer> map = new HashMap<>();
+4        for (int num : nums) {
+5            map.put(num, map.getOrDefault(num, 0) + 1);
+6        }
+7
+8        int ans = 0;
+9        for (int key : map.keySet()) {
+10            if (map.containsKey(key + 1)) {
+11                ans = Math.max(ans, map.get(key) + map.get(key + 1));
+12            }
+13        }
+14
+15        return ans;
+16    }
+17}
+18
