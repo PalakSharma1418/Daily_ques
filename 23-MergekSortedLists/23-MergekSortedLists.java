97f2fd5ec0ec6c676072b72211ece17b584bdd6c
Palak Sharma <palaksharma1418@gmail.com>
2025-12-21 12:37:22 +0530

    File updated at 12/21/2025, 12:35:40 PM

--- 23-MergekSortedLists/23-MergekSortedLists.java
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -1,40 +1,41 @@
-// Last updated: 8/22/2025, 7:33:18 PM
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>( new Comparator <>(){
-            // @Override
-            public int compare(ListNode o1 , ListNode o2){
-                return o1.val-o2.val;
-            }
-        });
-        ListNode Dummy = new ListNode();
-        ListNode temp=Dummy;
-        for(int i=0;i<lists.length;i++){
-            if(lists[i]!=null){
-                pq.add(lists[i]);
-            }
-        }
-
-        while(!pq.isEmpty()){
-            ListNode r=pq.poll();
-            Dummy.next=r;
-            Dummy = Dummy.next;
-            if(r.next!=null){
-                pq.add(r.next);
-            }
-        }
-        return temp.next;
-    }
-}
-
-// priority queue mai hum na address daal rhe hai bo phir compare nhi honge is we are using (a, b) -> a.val - b.val comparator
+// Last updated: 12/21/2025, 12:35:40 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeKLists(ListNode[] lists) {
+13        PriorityQueue<ListNode> pq = new PriorityQueue<>( new Comparator <>(){
+14            // @Override
+15            public int compare(ListNode o1 , ListNode o2){
+16                return o1.val-o2.val;
+17            }
+18        });
+19        ListNode Dummy = new ListNode();
+20        ListNode temp=Dummy;
+21        for(int i=0;i<lists.length;i++){
+22            if(lists[i]!=null){
+23                pq.add(lists[i]);
+24            }
+25        }
+26
+27        while(!pq.isEmpty()){
+28            ListNode r=pq.poll();
+29            Dummy.next=r;
+30            Dummy = Dummy.next;
+31            if(r.next!=null){
+32                pq.add(r.next);
+33            }
+34        }
+35        return temp.next;
+36    }
+37}
+38
+39// priority queue mai hum na address daal rhe hai bo phir compare nhi honge is we are using (a, b) -> a.val - b.val comparator
+40
