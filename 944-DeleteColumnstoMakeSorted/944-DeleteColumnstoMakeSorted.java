cd9dfc10aa3bc06c7412b94ba6b1cd8601c03097
Palak Sharma <palaksharma1418@gmail.com>
2025-12-22 16:07:05 +0530

    File updated at 12/22/2025, 4:05:23 PM

--- 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.java
+++ 944-DeleteColumnstoMakeSorted/944-DeleteColumnstoMakeSorted.java
@@ -1,17 +1,52 @@
-// Last updated: 12/20/2025, 5:06:53 PM
-1class Solution {
-2    public int minDeletionSize(String[] strs) {
-3        int res = 0;
-4        for (int j = 0; j < strs[0].length(); j++)
-5            res += isUnsorted(strs, j);
-6        return res;
-7    }
-8
-9    private int isUnsorted(String[] strs, int j) {
-10        for (int i = 1; i < strs.length; i++)
-11            if (strs[i].charAt(j) < strs[i - 1].charAt(j))
-12                return 1;
-13        return 0;
-14    }
-15}
-16
+// Last updated: 12/22/2025, 4:05:23 PM
+1// class Solution {
+2//     public int minDeletionSize(String[] strs) {
+3//         int count = 0;
+4//         int rows = strs.length;
+5//         int cols = strs[0].length();
+6//         for (int j = 0; j < cols; j++) {
+7//             for (int i = 0; i < rows - 1; i++) {
+8//                 if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
+9//                     count++;     
+10//                     break;       
+11//                 }
+12//             }
+13//         }
+14//         return count;
+15//     }
+16// }
+17class Solution {
+18    public int minDeletionSize(String[] strs) {
+19
+20        int n = strs.length;
+21        int m = strs[0].length();
+22        int deletions = 0;
+23
+24        boolean[] sorted = new boolean[n - 1];
+25
+26        for (int j = 0; j < m; j++) {
+27
+28            boolean badColumn = false;
+29
+30            // Step 1: check if column breaks order
+31            for (int i = 0; i < n - 1; i++) {
+32                if (!sorted[i] && strs[i].charAt(j) > strs[i + 1].charAt(j)) {
+33                    deletions++;
+34                    badColumn = true;
+35                    break;
+36                }
+37            }
+38
+39            // Step 2: update sorted pairs if column is good
+40            if (!badColumn) {
+41                for (int i = 0; i < n - 1; i++) {
+42                    if (!sorted[i] && strs[i].charAt(j) < strs[i + 1].charAt(j)) {
+43                        sorted[i] = true;
+44                    }
+45                }
+46            }
+47        }
+48        return deletions;
+49    }
+50}
+51
