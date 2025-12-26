946fb0a71ee75521f938880c4bf31d1c6b22b0f9
Palak Sharma <palaksharma1418@gmail.com>
2025-12-26 12:31:13 +0530

    File updated at 12/26/2025, 12:29:28 PM

--- 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -1,29 +1,29 @@
-// Last updated: 10/8/2025, 4:45:24 PM
-class Solution {
-    public long minimumTime(int[] time, int totalTrips) {
-        long ans=0;
-        int max = 0;
-        for(int n:time){
-            max=Math.max(n,max);
-        }
-        long low = 1, high = (long) max * totalTrips;
-        while(low<=high){
-            long mid=low+(high-low)/2;
-            if(ispossible(time,mid,totalTrips)){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return ans;
-    }
-    public boolean ispossible(int[] time, long t, int trip) {
-    long sum = 0;
-    for (int i = 0; i < time.length; i++) {
-        sum += t / time[i];
-        if (sum >= trip) return true; 
-    }
-    return sum >= trip;
-}
-}
+// Last updated: 12/26/2025, 12:29:28 PM
+1class Solution {
+2    public long minimumTime(int[] time, int totalTrips) {
+3        long ans=0;
+4        int max = 0;
+5        for(int n:time){
+6            max=Math.max(n,max);
+7        }
+8        long low = 1, high = (long) max * totalTrips;
+9        while(low<=high){
+10            long mid=low+(high-low)/2;
+11            if(ispossible(time,mid,totalTrips)){
+12                ans=mid;
+13                high=mid-1;
+14            }else{
+15                low=mid+1;
+16            }
+17        }
+18        return ans;
+19    }
+20    public boolean ispossible(int[] time, long t, int trip) {
+21    long sum = 0;
+22    for (int i = 0; i < time.length; i++) {
+23        sum += t / time[i];
+24        if (sum >= trip) return true; 
+25    }
+26    return sum >= trip;
+27}
+28}
