14a9232ef4d56deba1f8e3a55f67619bbaa90095
Palak Sharma <palaksharma1418@gmail.com>
2025-12-30 10:40:56 +0530

    File updated at 12/30/2025, 10:39:10 AM

--- 997-FindtheTownJudge/997-FindtheTownJudge.java
+++ 997-FindtheTownJudge/997-FindtheTownJudge.java
@@ -1,17 +1,17 @@
-// Last updated: 10/8/2025, 7:46:08 AM
-class Solution {
-    public int findJudge(int n, int[][] trust) {
-        if (trust.length == 0 && n == 1) 
-            return 1;
-        int[] count = new int[n + 1];
-        for (int[] person : trust) {
-            count[person[0]]--;
-            count[person[1]]++;
-        }
-
-        for (int person = 0; person < count.length; person++) {
-            if (count[person] == n - 1) return person;
-        }
-        return -1;
-    }
-}
+// Last updated: 12/30/2025, 10:39:10 AM
+1class Solution {
+2    public int findJudge(int n, int[][] trust) {
+3        if (trust.length == 0 && n == 1) 
+4            return 1;
+5        int[] count = new int[n + 1];
+6        for (int[] person : trust) {
+7            count[person[0]]--;
+8            count[person[1]]++;
+9        }
+10
+11        for (int person = 0; person < count.length; person++) {
+12            if (count[person] == n - 1) return person;
+13        }
+14        return -1;
+15    }
+16}
