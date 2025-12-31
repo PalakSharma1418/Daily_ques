a510121d845c0132d9737755a4b6d501f9c4ceec
Palak Sharma <palaksharma1418@gmail.com>
2025-12-31 19:15:17 +0530

    File updated at 12/31/2025, 7:13:31 PM

--- 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
@@ -1,36 +1,36 @@
-// Last updated: 11/22/2025, 9:49:19 AM
-class Solution {
-    
-    private HashMap<Integer,HashMap<Integer,Integer>> map;
-    public void AddEdge(int v1, int v2, int cost) {
-		map.get(v1).put(v2, cost);
-		map.get(v2).put(v1, cost);
-	}
-    public boolean hashpath(int src, int dis, HashSet<Integer> visited) {
-		if (src == dis) {
-			return true;
-		}
-		visited.add(src);
-		for (int nbrs : map.get(src).keySet()) {
-			if (!visited.contains(nbrs)) {
-				boolean ans = hashpath(nbrs, dis, visited);
-				if (ans) {
-					return ans;
-				}
-			}
-		}
-		// visited.remove(src); 
-		return false;
-	}
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(i, new HashMap<>());
-        }
-        for (int[] e : edges) {
-            AddEdge(e[0], e[1], 1);
-        }
-        return hashpath(source, destination, new HashSet<>());
-    }
-
-}
+// Last updated: 12/31/2025, 7:13:31 PM
+1class Solution {
+2    
+3    private HashMap<Integer,HashMap<Integer,Integer>> map;
+4    public void AddEdge(int v1, int v2, int cost) {
+5		map.get(v1).put(v2, cost);
+6		map.get(v2).put(v1, cost);
+7	}
+8    public boolean hashpath(int src, int dis, HashSet<Integer> visited) {
+9		if (src == dis) {
+10			return true;
+11		}
+12		visited.add(src);
+13		for (int nbrs : map.get(src).keySet()) {
+14			if (!visited.contains(nbrs)) {
+15				boolean ans = hashpath(nbrs, dis, visited);
+16				if (ans) {
+17					return ans;
+18				}
+19			}
+20		}
+21		// visited.remove(src); 
+22		return false;
+23	}
+24    public boolean validPath(int n, int[][] edges, int source, int destination) {
+25        map = new HashMap<>();
+26        for (int i = 0; i < n; i++) {
+27            map.put(i, new HashMap<>());
+28        }
+29        for (int[] e : edges) {
+30            AddEdge(e[0], e[1], 1);
+31        }
+32        return hashpath(source, destination, new HashSet<>());
+33    }
+34
+35}
