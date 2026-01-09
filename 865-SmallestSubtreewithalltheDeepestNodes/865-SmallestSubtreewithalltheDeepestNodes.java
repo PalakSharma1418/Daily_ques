55e80578d5d61bedfa860e9c5a030d01ba5c45de
Palak Sharma <palaksharma1418@gmail.com>
2026-01-09 18:07:47 +0530

    Create 865-SmallestSubtreewithalltheDeepestNodes

---
+++ 865-SmallestSubtreewithalltheDeepestNodes/865-SmallestSubtreewithalltheDeepestNodes.java
@@ -0,0 +1,46 @@
+// Last updated: 1/9/2026, 6:05:57 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    HashMap<TreeNode,Integer> map = new HashMap<>();
+18    int maxd = 0;
+19
+20    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+21        depth(root, 0);
+22        return lca(root);
+23    }
+24
+25    void depth(TreeNode root, int d) {
+26        if (root == null) return;
+27        map.put(root, d);
+28        maxd = Math.max(maxd, d);
+29        depth(root.left, d + 1);
+30        depth(root.right, d + 1);
+31    }
+32
+33    TreeNode lca(TreeNode root) {
+34        if (root == null || map.get(root) == maxd) {
+35            return root;
+36        }
+37
+38        TreeNode left = lca(root.left);
+39        TreeNode right = lca(root.right);
+40
+41        if (left != null && right != null) return root;
+42        return left != null ? left : right;
+43    }
+44}
+45
