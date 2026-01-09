773f50728d5d37189dc8da1bd634b0c99de7d05d
Palak Sharma <palaksharma1418@gmail.com>
2026-01-09 10:59:02 +0530

    Create 1496-LuckyNumbersInAMatrix

---
+++ 1496-LuckyNumbersInAMatrix/1496-LuckyNumbersInAMatrix.java
@@ -0,0 +1,34 @@
+// Last updated: 1/9/2026, 10:57:12 AM
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        List<Integer> ll = new ArrayList<>();
+        int[] row = new int[matrix.length];
+        int [] col = new int[matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            int min=Integer.MAX_VALUE;
+            for(int j=0;j<matrix[0].length;j++){
+                if(min> matrix[i][j]){
+                    min=matrix[i][j];
+                }
+            }
+            row[i]=min;
+        }
+        for(int j=0;j<matrix[0].length;j++){
+            int max=Integer.MIN_VALUE;
+            for(int i=0;i<matrix.length;i++){
+                if(max< matrix[i][j]){
+                    max=matrix[i][j];
+                }
+            }
+            col[j]=max;
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==row[i] && matrix[i][j]==col[j]){
+                    ll.add(matrix[i][j]);
+                }
+            }
+        }
+        return ll;
+    }
+}
