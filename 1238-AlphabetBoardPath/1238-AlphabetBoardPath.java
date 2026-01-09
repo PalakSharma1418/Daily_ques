6aba6e870de60642b0023139f78f88afb8f2693b
Palak Sharma <palaksharma1418@gmail.com>
2026-01-09 10:59:34 +0530

    Create 1238-AlphabetBoardPath

---
+++ 1238-AlphabetBoardPath/1238-AlphabetBoardPath.java
@@ -0,0 +1,30 @@
+// Last updated: 1/9/2026, 10:57:44 AM
+class Solution {
+    public String alphabetBoardPath(String target) {
+        StringBuilder sb=new StringBuilder();
+        int x=0;
+        int y=0;
+        for(char c:target.toCharArray()){
+            int x1=(c-'a')/5;
+            int y1=(c-'a')%5;
+            while(x>x1){
+                sb.append('U');
+                x--;
+            }
+            while(y<y1){
+                sb.append('R');
+                y++;
+            }
+            while(y>y1){
+                sb.append('L');
+                y--;
+            }
+            while(x<x1){
+                sb.append('D');
+                x++;
+            }
+            sb.append('!');
+        }
+        return sb.toString();
+    }
+}
