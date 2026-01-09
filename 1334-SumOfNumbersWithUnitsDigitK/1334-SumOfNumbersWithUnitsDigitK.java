7357989b834b42743ae3c3c58b35dc579e52b839
Palak Sharma <palaksharma1418@gmail.com>
2026-01-09 10:59:19 +0530

    Create 1334-SumOfNumbersWithUnitsDigitK

---
+++ 1334-SumOfNumbersWithUnitsDigitK/1334-SumOfNumbersWithUnitsDigitK.java
@@ -0,0 +1,17 @@
+// Last updated: 1/9/2026, 10:57:29 AM
+class Solution{
+    public int minimumNumbers(int num, int k){
+        if(num == 0)
+            return 0;
+        if(k == 0)
+            if(num % 10 == 0) 
+                return 1;
+            else
+                return -1;
+        for(int i = 1; i <= num/k; i++) 
+            if(num % 10 == ((i*k)%10)) 
+                return i;
+        
+        return -1;
+    }
+}
