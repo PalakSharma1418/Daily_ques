b6b6fd8ebb3c5f24a3648a5e78ab142df2b69def
Palak Sharma <palaksharma1418@gmail.com>
2026-01-12 17:42:16 +0530

    Create 3719-LongestBalancedSubarrayI

---
+++ 3719-LongestBalancedSubarrayI/3719-LongestBalancedSubarrayI.java
@@ -0,0 +1,23 @@
+// Last updated: 1/12/2026, 5:40:24 PM
+1class Solution {
+2    public int longestBalanced(int[] nums) {
+3        int no = nums.length;
+4        int ans=0;
+5        for(int l=0;l<no;l++){
+6            Map<Integer,Integer> even = new HashMap<>();
+7            Map<Integer,Integer> odd = new HashMap<>();
+8            for(int r=l;r<no;r++){
+9                int n= nums[r];
+10                if(n%2==0){
+11                    even.put(n,even.getOrDefault(n,0)+1);
+12                }else{
+13                    odd.put(n,odd.getOrDefault(n,0)+1);
+14                }
+15                if(even.size()==odd.size()){
+16                    ans=Math.max(ans,r-l+1);
+17                }
+18            }
+19        }
+20        return ans;
+21    }
+22}
