747aa245f1bffac03101f12d5e92bf647de53820
Palak Sharma <palaksharma1418@gmail.com>
2026-01-16 20:10:14 +0530

    Create 3634-MinimumRemovalstoBalanceArray

---
+++ 3634-MinimumRemovalstoBalanceArray/3634-MinimumRemovalstoBalanceArray.java
@@ -0,0 +1,21 @@
+// Last updated: 1/16/2026, 8:08:22 PM
+1class Solution {
+2    public int minRemoval(int[] nums, int k) {
+3        Arrays.sort(nums);
+4        int n = nums.length;
+5
+6        int i = 0;
+7        int maxLen = 0;
+8
+9        for (int j = 0; j < n; j++) {
+10            while ((long) nums[j] > (long) nums[i] * k) {
+11                i++;
+12            }
+13            maxLen = Math.max(maxLen, j - i + 1);
+14        }
+15
+16        return n - maxLen;
+17    }
+18}
+19
+20
