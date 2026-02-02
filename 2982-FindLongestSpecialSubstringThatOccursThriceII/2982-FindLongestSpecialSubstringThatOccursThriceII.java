447ba14ececea8c07600b5c481f94dc8173ce5a6
Palak Sharma <palaksharma1418@gmail.com>
2026-02-02 12:53:43 +0530

    File updated at 2/2/2026, 12:51:44 PM

--- 2982-FindLongestSpecialSubstringThatOccursThriceII/2982-FindLongestSpecialSubstringThatOccursThriceII.java
+++ 2982-FindLongestSpecialSubstringThatOccursThriceII/2982-FindLongestSpecialSubstringThatOccursThriceII.java
@@ -1,34 +1,39 @@
-// Last updated: 1/20/2026, 4:03:08 PM
+// Last updated: 2/2/2026, 12:51:44 PM
 1class Solution {
 2    public int maximumLength(String s) {
-3        int n=s.length();
-4        int[] len=new int[n];
-5        len[0]=1;
-6        for(int i=1;i<n;i++){
-7            if(s.charAt(i)==s.charAt(i-1)){
-8                len[i]=len[i-1]+1;
-9            }else{
-10                len[i]=1;
-11            }
-12        }
-13        HashMap<Character,List<Integer>> map=new HashMap<>();
-14        for(int i=0;i<n;i++){
-15            if(map.containsKey(s.charAt(i))){
-16                map.get(s.charAt(i)).add(len[i]);
-17            }else{
-18                map.put(s.charAt(i),new ArrayList<>());
-19                map.get(s.charAt(i)).add(len[i]);
-20            }
-21        }
-22        for(List<Integer> ls:map.values()){
-23            Collections.sort(ls,Collections.reverseOrder());
-24        }
-25        int ans=-1;
-26        for(List<Integer> ls:map.values()){
-27            if(ls.size()>=3){
-28                ans=Math.max(ans,ls.get(2));
-29            }
-30        }
-31        return ans;
-32    }
-33}
+3        int n = s.length();
+4        int[][] matrix = new int[26][n + 1]; // Matrix to store counts for each character and length
+5        char prevChar = s.charAt(0);
+6        int length = 0;
+7
+8        // Populate the matrix
+9        for (int i = 0; i < n; i++) {
+10            char currChar = s.charAt(i);
+11
+12            if (currChar == prevChar) {
+13                length += 1;
+14                matrix[currChar - 'a'][length] += 1;
+15            } else {
+16                length = 1;
+17                matrix[currChar - 'a'][length] += 1;
+18                prevChar = currChar;
+19            }
+20        }
+21
+22        int result = 0;
+23
+24        // Calculate the result by checking cumulative sums in reverse order
+25        for (int i = 0; i < 26; i++) {
+26            int cumSum = 0;
+27            for (int j = n; j >= 1; j--) {
+28                cumSum += matrix[i][j];
+29                if (cumSum >= 3) {
+30                    result = Math.max(result, j);
+31                    break;
+32                }
+33            }
+34        }
+35
+36        return result == 0 ? -1 : result;
+37    }
+38}
