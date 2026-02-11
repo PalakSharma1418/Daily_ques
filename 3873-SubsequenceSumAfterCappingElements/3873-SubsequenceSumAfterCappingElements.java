5613ed71610e16d0d852d54808e13e3bae1f1dad
Palak Sharma <palaksharma1418@gmail.com>
2026-02-11 07:22:55 +0530

    Create 3873-SubsequenceSumAfterCappingElements

---
+++ 3873-SubsequenceSumAfterCappingElements/3873-SubsequenceSumAfterCappingElements.java
@@ -0,0 +1,32 @@
+// Last updated: 2/11/2026, 7:20:52 AM
+class Solution {
+    public boolean[] subsequenceSumAfterCapping(int[] nums, int k) {
+        int n = nums.length;
+        boolean[] ans = new boolean[n];
+        Arrays.sort(nums);
+        boolean[] dp = new boolean[k + 1];
+        dp[0] = true;
+        int i = 0;
+
+        for (int x = 1; x <= n; x++) {
+            for (; i < n && nums[i] < x; i++) {
+                for (int j = k; j >= nums[i]; j--) {
+                    dp[j] = dp[j] || dp[j - nums[i]];
+                }
+            }
+            int ncapped = n - i;
+            for (int j = 0; j <= ncapped; j++) {
+                int times = j * x;
+                if (times > k) {
+                    break;
+                }
+                if (dp[k - times]) {
+                    ans[x - 1] = true;
+                    break;
+                }
+            }
+
+        }
+        return ans;
+    }
+}
