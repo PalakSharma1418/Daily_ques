0d55c66c1768354996251e6891f88b6f1867851d
Palak Sharma <palak.sharma_cs23@gla.ac.in>
2026-07-14 17:56:03 +0530

    File updated at 7/14/2026, 5:55:58 PM

--- 386-LexicographicalNumbers/386-LexicographicalNumbers.java
+++ 386-LexicographicalNumbers/386-LexicographicalNumbers.java
@@ -1,34 +1,34 @@
-// Last updated: 8/6/2025, 4:48:19 PM
-class Solution {
-    public List<Integer> lexicalOrder(int n) {
-        
-        List<Integer> ll= new ArrayList<>();
-        if(n==1){
-            ll.add(1);
-            return ll;
-        }
-        if(n==2){
-            ll.add(1);
-            ll.add(2);
-            return ll;
-        }
-        print(n,0,ll);
-        return ll;
-    }
-    public static void print(int  n,int curr,List<Integer> ll){
-        if(curr>n){
-            return;
-        }
-        if(curr !=0 ){
-            // System.out.println(curr);
-            ll.add(curr);
-        }
-        int i=0;
-        if(curr==0){
-            i=1;
-        }
-        for(;i<=9;i++){
-            print(n, curr*10+i,ll);
-        }
-    }
-}
+// Last updated: 7/14/2026, 5:55:58 PM
+1class Solution {
+2    public List<Integer> lexicalOrder(int n) {
+3        
+4        List<Integer> ll= new ArrayList<>();
+5        if(n==1){
+6            ll.add(1);
+7            return ll;
+8        }
+9        if(n==2){
+10            ll.add(1);
+11            ll.add(2);
+12            return ll;
+13        }
+14        print(n,0,ll);
+15        return ll;
+16    }
+17    public static void print(int  n,int curr,List<Integer> ll){
+18        if(curr>n){
+19            return;
+20        }
+21        if(curr !=0 ){
+22            // System.out.println(curr);
+23            ll.add(curr);
+24        }
+25        int i=0;
+26        if(curr==0){
+27            i=1;
+28        }
+29        for(;i<=9;i++){
+30            print(n, curr*10+i,ll);
+31        }
+32    }
+33}
