ba79cf8ba200b69afc68a5d683b9ba7c81493f31
Palak Sharma <palak.sharma_cs23@gla.ac.in>
2026-07-26 18:42:58 +0530

    File updated at 7/26/2026, 6:42:53 PM

--- 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.java
+++ 628-MaximumProductofThreeNumbers/628-MaximumProductofThreeNumbers.java
@@ -1,28 +1,28 @@
-// Last updated: 9/27/2025, 1:16:08 PM
-class Solution {
-    public int maximumProduct(int[] nums) {
-         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
-        int  min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
-        for (int n : nums) {
-            if (n > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = n;
-            } else if (n > max2) {
-                max3 = max2;
-                max2 = n;
-            } else if (n > max3) {
-                max3 = n;
-            }
-
-            if (n < min1) {
-                min2 = min1;
-                min1 = n;
-            } else if (n < min2) {
-                min2 = n;
-            }
-        }
-        // agar negative ho
-        return Math.max(max1*max2*max3, max1*min1*min2);
-    }
-}
+// Last updated: 7/26/2026, 6:42:53 PM
+1class Solution {
+2    public int maximumProduct(int[] nums) {
+3         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+4        int  min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+5        for (int n : nums) {
+6            if (n > max1) {
+7                max3 = max2;
+8                max2 = max1;
+9                max1 = n;
+10            } else if (n > max2) {
+11                max3 = max2;
+12                max2 = n;
+13            } else if (n > max3) {
+14                max3 = n;
+15            }
+16
+17            if (n < min1) {
+18                min2 = min1;
+19                min1 = n;
+20            } else if (n < min2) {
+21                min2 = n;
+22            }
+23        }
+24        // agar negative ho
+25        return Math.max(max1*max2*max3, max1*min1*min2);
+26    }
+27}
